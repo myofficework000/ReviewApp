acc07793c9054e285b064088d81ed29fb8aaff4f
myofficework000 <cheetahmail007@gmail.com>
2025-06-12 03:38:27 +0530

    Updated the settings screen

--- app/src/main/java/com/code4galaxy/reviewnow/view/feature/user/settings/UserSettingsScreen.kt
+++ app/src/main/java/com/code4galaxy/reviewnow/view/feature/user/settings/UserSettingsScreen.kt
@@ -2,12 +2,13 @@ package com.code4galaxy.reviewnow.view.feature.user.settings
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun UserSettingsScreen(modifier: Modifier = Modifier) {
     Box(modifier.fillMaxSize()) {
-
+        Text("Settings")
     }
 }
