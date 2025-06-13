e40709e82d6d513716cd861f23127f0aa36db8dd
myofficework000 <cheetahmail007@gmail.com>
2025-06-14 00:26:35 +0530

    localization

--- app/src/main/java/com/code4galaxy/reviewnow/model/DrawerItem.kt
+++ app/src/main/java/com/code4galaxy/reviewnow/model/DrawerItem.kt
@@ -2,14 +2,6 @@ package com.code4galaxy.reviewnow.model
 
 import com.code4galaxy.reviewnow.view.navigation.Screen
 
-val drawerItems = listOf(
-    DrawerItem.Home,
-    DrawerItem.MyReviews,
-    DrawerItem.Profile
-)
-
-
-
 sealed class DrawerItem(val title: String, val route: String) {
     object Home : DrawerItem("Home", Screen.Home.route)
     object MyReviews : DrawerItem("My Reviews", Screen.MyReviews.route)
